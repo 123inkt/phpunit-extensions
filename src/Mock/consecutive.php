c46c7d1b5413e9e218023c2c8fa984902cb46d0a
Frank Dekker <fdekker@123inkt.nl>
2023-07-06 10:53:58 +0200

    Ensure function is only loaded once

--- src/Mock/consecutive.php
+++ src/Mock/consecutive.php
@@ -8,47 +8,49 @@ use InvalidArgumentException;
 use PHPUnit\Framework\Constraint\Callback;
 use PHPUnit\Framework\Constraint\IsAnything;
 
-/**
- * @param array<mixed> $firstInvocationArguments A list of arguments for each invocation that will be asserted against.
- *                                               Will fail on: too many invocations or if the argument doesn't match for each invocation.
- * @param array<mixed> $secondInvocationArguments
- * @param array<mixed> ...$expectedArgumentList
- *
- * @note    Full qualified name to appease to phpstorm inspection gods
- *  phpcs:ignore
- * @return \PHPUnit\Framework\Constraint\Callback<mixed>[]
- * @example <code>->with(...consecutive([5, 'foo'], [6, 'bar']))</code>
- */
-function consecutive(array $firstInvocationArguments, array $secondInvocationArguments, array ...$expectedArgumentList): array
-{
-    array_unshift($expectedArgumentList, $secondInvocationArguments);
-    array_unshift($expectedArgumentList, $firstInvocationArguments);
+if (function_exists('\DR\PHPUnitExtensions\Mock\consecutive') === false) {
+    /**
+     * @param array<mixed> $firstInvocationArguments A list of arguments for each invocation that will be asserted against.
+     *                                               Will fail on: too many invocations or if the argument doesn't match for each invocation.
+     * @param array<mixed> $secondInvocationArguments
+     * @param array<mixed> ...$expectedArgumentList
+     *
+     * @note    Full qualified name to appease to phpstorm inspection gods
+     *  phpcs:ignore
+     * @return \PHPUnit\Framework\Constraint\Callback<mixed>[]
+     * @example <code>->with(...consecutive([5, 'foo'], [6, 'bar']))</code>
+     */
+    function consecutive(array $firstInvocationArguments, array $secondInvocationArguments, array ...$expectedArgumentList): array
+    {
+        array_unshift($expectedArgumentList, $secondInvocationArguments);
+        array_unshift($expectedArgumentList, $firstInvocationArguments);
 
-    // count arguments
-    $maxArguments = (int)max(array_map(static fn($args) => count($args), $expectedArgumentList));
-    if ($maxArguments === 0) {
-        throw new InvalidArgumentException('consecutive() is expecting at least 1 or more arguments for invocation');
-    }
-
-    // reorganize arguments per argument index
-    $argumentsByIndex = [];
-    foreach ($expectedArgumentList as $invocation => $expectedArguments) {
-        foreach ($expectedArguments as $index => $argument) {
-            $argumentsByIndex[$index][$invocation] = $argument;
+        // count arguments
+        $maxArguments = (int)max(array_map(static fn($args) => count($args), $expectedArgumentList));
+        if ($maxArguments === 0) {
+            throw new InvalidArgumentException('consecutive() is expecting at least 1 or more arguments for invocation');
         }
-    }
 
-    $callbacks = [];
-    /** @var array<int, mixed> $arguments */
-    foreach ($argumentsByIndex as $arguments) {
-        for ($i = 0; $i < $maxArguments; $i++) {
-            if (isset($arguments[$i]) === false) {
-                $arguments[$i] = new IsAnything();
+        // reorganize arguments per argument index
+        $argumentsByIndex = [];
+        foreach ($expectedArgumentList as $invocation => $expectedArguments) {
+            foreach ($expectedArguments as $index => $argument) {
+                $argumentsByIndex[$index][$invocation] = $argument;
             }
         }
-        $constraint  = new ConsecutiveParameters($arguments);
-        $callbacks[] = new Callback(static fn($actualArgument): bool => $constraint->evaluate($actualArgument));
-    }
 
-    return $callbacks;
+        $callbacks = [];
+        /** @var array<int, mixed> $arguments */
+        foreach ($argumentsByIndex as $arguments) {
+            for ($i = 0; $i < $maxArguments; $i++) {
+                if (isset($arguments[$i]) === false) {
+                    $arguments[$i] = new IsAnything();
+                }
+            }
+            $constraint  = new ConsecutiveParameters($arguments);
+            $callbacks[] = new Callback(static fn($actualArgument): bool => $constraint->evaluate($actualArgument));
+        }
+
+        return $callbacks;
+    }
 }
