2e95a102ec005b124b6b8ffaf9f32db2ae1853d6
Frank Dekker <fdekker@123inkt.nl>
2025-01-02 11:11:10 +0100

    Add documentation

--- src/Symfony/ClockTestTrait.php
+++ src/Symfony/ClockTestTrait.php
@@ -20,23 +20,32 @@ trait ClockTestTrait
     }
 
     /**
-     * Override this method to set your own custom fix time freeze.
+     * Override this method to set your own custom time freeze.
      */
     protected function freezeTimeAt(): int
     {
         return 1634050575; // tuesday, 12-10-2021 16:56:15 +02:00
     }
 
+    /**
+     * Returns the currently mocked time as timestamp
+     */
     protected static function time(): int
     {
         return Clock::get()->now()->getTimestamp();
     }
 
+    /**
+     * Returns the currently mocked time as DateTimeImmutable object
+     */
     protected static function now(): DateTimeImmutable
     {
         return Clock::get()->now();
     }
 
+    /**
+     * Sleep for a given amount of seconds. Use a float to sleep fractions of a second.
+     */
     protected static function sleep(int|float $seconds): void
     {
         Clock::get()->sleep($seconds);
