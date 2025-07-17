37d092dbdf8a77f03c6210654509dc77594f3344
Frank Dekker <fdekker@123inkt.nl>
2025-07-17 23:06:50 +0200

    Add is same image constraint

---
+++ src/Constraint/IsSameImageConstraint.php
@@ -0,0 +1,147 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DR\PHPUnitExtensions\Constraint;
+
+use Closure;
+use Imagick;
+use ImagickException;
+use PHPUnit\Framework\Constraint\Constraint;
+use PHPUnit\Framework\ExpectationFailedException;
+use SplFileInfo;
+
+class IsSameImageConstraint extends Constraint
+{
+    private ?string $additionalInfo = null;
+
+    /**
+     * @param string|SplFileInfo|resource                                             $expectedImage
+     * @param (Closure(Imagick $diff, Imagick $expected, Imagick $actual): void)|null $diffCallback
+     */
+    public function __construct(private $expectedImage, private ?Closure $diffCallback = null)
+    {
+    }
+
+    /**
+     * @param string|SplFileInfo|resource $other Pass either binary data string, a file path or a resource handle
+     *
+     * @throws ExpectationFailedException
+     */
+    public function matches(mixed $other): bool
+    {
+        if (class_exists('Imagick') === false) {
+            $this->fail($this->getFileName($other), "IsSameImageConstraint requires Imagick extension to be installed.");
+        }
+
+        try {
+            $expected = $this->getImageObject($this->expectedImage);
+            $actual   = $this->getImageObject($other);
+
+            $expectedImageCount = $expected->getNumberImages();
+            $actualImageCount   = $actual->getNumberImages();
+            if ($expectedImageCount !== $actualImageCount) {
+                $this->fail(
+                    $this->getFileName($other),
+                    "Expected an image count of " . $expectedImageCount . " but received count of " . $actualImageCount,
+                );
+            }
+
+            $success = true;
+            for ($index = 0; $index < $expectedImageCount; ++$index) {
+                $expected->setIteratorIndex($index);
+                $actual->setIteratorIndex($index);
+
+                $success = $this->compareImages($expected, $actual);
+                if ($success === false) {
+                    break;
+                }
+            }
+        } catch (ImagickException $e) {
+            $this->fail($this->getFileName($other), "Image compare failed due to Imagick error: " . $e->getMessage());
+        }
+
+        return $success;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function toString(): string
+    {
+        return "matches " . $this->getFileName($this->expectedImage);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function additionalFailureDescription(mixed $other): string
+    {
+        return $this->additionalInfo ?? parent::additionalFailureDescription($other);
+    }
+
+    /**
+     * @param string|SplFileInfo|resource $data
+     *
+     * @throws ImagickException
+     */
+    private function getImageObject($data): Imagick
+    {
+        if ($data instanceof SplFileInfo) {
+            // create resource from file
+            $handle = fopen($data->getPathname(), 'rb');
+            assert(is_resource($handle));
+        } elseif (is_string($data)) {
+            // create resource from data string
+            $handle = fopen('php://memory', 'rb+');
+            assert(is_resource($handle));
+            fwrite($handle, $data);
+            fseek($handle, 0);
+        } else {
+            $handle = $data;
+        }
+
+        $image = new Imagick();
+        $image->readImageFile($handle);
+        $image->resetIterator();
+
+        return $image;
+    }
+
+    /**
+     * @throws ImagickException
+     */
+    private function compareImages(Imagick $baseImage, Imagick $generatedImage): bool
+    {
+        [$imageDiff, $difference] = $baseImage->compareImages($generatedImage, 1);
+        if ($difference === 0.0) {
+            return true;
+        }
+
+        // call callback if present
+        if ($this->diffCallback !== null) {
+            ($this->diffCallback)($imageDiff, $baseImage, $generatedImage);
+        }
+
+        // report findings
+        $this->additionalInfo = 'Imagick found a difference of ' . $difference . '.';
+
+        return false;
+    }
+
+    /**
+     * @param string|SplFileInfo|resource $data
+     */
+    private function getFileName($data): string
+    {
+        if ($data instanceof SplFileInfo) {
+            return $data->getPathname();
+        }
+
+        if (is_resource($data)) {
+            return stream_get_meta_data($data)['uri'];
+        }
+
+        return 'binary-data-stream';
+    }
+}
