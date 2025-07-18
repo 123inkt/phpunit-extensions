2697e51869855cff4912744974874c9c4e2a555f
Frank Dekker <fdekker@123inkt.nl>
2025-07-18 22:07:35 +0200

    Add ImageAsserter trait

---
+++ src/Trait/ImageAsserter.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DR\PHPUnitExtensions\Trait;
+
+use DR\PHPUnitExtensions\Constraint\IsSameImageConstraint;
+use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\Constraint\LogicalNot;
+use PHPUnit\Framework\Exception;
+use SplFileInfo;
+
+trait ImageAsserter
+{
+    /**
+     * @param string|SplFileInfo|resource $expected Pass either binary data string, a file path or a resource handle
+     * @param string|SplFileInfo|resource $actual Pass either binary data string, a file path or a resource handle
+     *
+     * @throws Exception
+     */
+    public function assertSameImage($expected, $actual, string $message = ''): void
+    {
+        Assert::assertThat($actual, $this->getConstraint($expected), $message);
+    }
+
+    /**
+     * @param string|SplFileInfo|resource $expected Pass either binary data string, a file path or a resource handle
+     * @param string|SplFileInfo|resource $actual Pass either binary data string, a file path or a resource handle
+     *
+     * @throws Exception
+     */
+    public function assertNotSameImage($expected, $actual, string $message = ''): void
+    {
+        Assert::assertThat($actual, new LogicalNot($this->getConstraint($expected)), $message);
+    }
+
+    /**
+     * @param resource $expectedHandle
+     */
+    abstract protected function getConstraint($expectedHandle): IsSameImageConstraint;
+}
