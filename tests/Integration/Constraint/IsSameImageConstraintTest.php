69ed0f2fb8148e174fba6fdf2162f172146d2168
Frank Dekker <fdekker@123inkt.nl>
2025-07-21 13:52:36 +0200

    Add coverage

--- tests/Integration/Constraint/IsSameImageConstraintTest.php
+++ tests/Integration/Constraint/IsSameImageConstraintTest.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace DR\PHPUnitExtensions\Tests\Integration\Constraint;
 
+use Closure;
 use DR\PHPUnitExtensions\Constraint\IsSameImageConstraint;
 use DR\PHPUnitExtensions\Trait\ImageTestTrait;
+use Imagick;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 use SplFileInfo;
@@ -18,12 +20,14 @@ class IsSameImageConstraintTest extends TestCase
 
     private string $imageA;
     private string $imageB;
+    private ?Closure $callback = null;
 
     protected function setUp(): void
     {
         parent::setUp();
-        $this->imageA = dirname(__DIR__, 2) . '/Resources/Constraint/white-a.png';
-        $this->imageB = dirname(__DIR__, 2) . '/Resources/Constraint/white-b.png';
+        $this->callback = null;
+        $this->imageA   = dirname(__DIR__, 2) . '/Resources/Constraint/white-a.png';
+        $this->imageB   = dirname(__DIR__, 2) . '/Resources/Constraint/white-b.png';
     }
 
     public function testAssertSplFileInfo(): void
@@ -59,5 +63,30 @@ class IsSameImageConstraintTest extends TestCase
 
         static::assertSameImage($dataA1, $dataA2);
         static::assertNotSameImage($dataA1, $dataB);
+    }
+
+    public function testAssertWithCallback(): void
+    {
+        $fileA           = new SplFileInfo($this->imageA);
+        $fileB           = new SplFileInfo($this->imageB);
+        $callbackInvoked = false;
+
+        $this->callback = function ($diff, $expected, $actual) use (&$callbackInvoked) : void {
+            static::assertInstanceOf(Imagick::class, $diff);
+            static::assertInstanceOf(Imagick::class, $expected);
+            static::assertInstanceOf(Imagick::class, $actual);
+            $callbackInvoked = true;
+        };
+
+        static::assertNotSameImage($fileA, $fileB);
+        static::assertTrue($callbackInvoked);
+    }
+
+    /**
+     * @param string|SplFileInfo|resource $expectedHandle
+     */
+    protected function getConstraint($expectedHandle): IsSameImageConstraint
+    {
+        return new IsSameImageConstraint($expectedHandle, $this->callback);
     }
 }
