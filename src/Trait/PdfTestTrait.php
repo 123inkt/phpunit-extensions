08109f6087dc53b4fefe158bcaa3d581ac1d0015
Frank Dekker <fdekker@123inkt.nl>
2025-07-21 20:49:30 +0200

    Add PdfTestTrait

--- src/Trait/PdfTestTrait.php
+++ src/Trait/PdfTestTrait.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace DR\PHPUnitExtensions\Trait;
 
 use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\Constraint\LogicalNot;
 use SplFileInfo;
 use TCPDF;
 
@@ -22,5 +23,17 @@ trait PdfTestTrait
         }
 
         Assert::assertThat($actual->Output('', 'S'), $this->getConstraint($expected), $message);
+    }
+
+    /**
+     * @param string|SplFileInfo|resource|TCPDF $expected binary data string, a file path, a resource handle or a TCPDF instance to compare to
+     */
+    public function assertNotSamePDF($expected, TCPDF $actual, string $message = ''): void
+    {
+        if ($expected instanceof TCPDF) {
+            $expected = $expected->Output('', 'S');
+        }
+
+        Assert::assertThat($actual->Output('', 'S'), new LogicalNot($this->getConstraint($expected)), $message);
     }
 }
