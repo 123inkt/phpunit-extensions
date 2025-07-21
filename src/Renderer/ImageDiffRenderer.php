dde3349db81efe6dee625e94862532d9d057ed2e
Frank Dekker <fdekker@123inkt.nl>
2025-07-21 21:54:57 +0200

    Add ImageDiffRenderer

---
+++ src/Renderer/ImageDiffRenderer.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DR\PHPUnitExtensions\Renderer;
+
+use Imagick;
+use ImagickException;
+
+/**
+ * Renders the results IsSameImageConstraint difference to local html file. Can be enabled by setting the `PHPUNIT_EXTENSIONS_IMAGE_DIFF_OUTPUT_PATH`
+ * @internal
+ */
+class ImageDiffRenderer
+{
+    private readonly ?string $outputPath;
+
+    public function __construct(?string $outputPath = null)
+    {
+        $this->outputPath = $outputPath ?? $_SERVER['PHPUNIT_EXTENSIONS_IMAGE_DIFF_OUTPUT_PATH'] ?? null;
+    }
+
+    /**
+     * @throws ImagickException
+     */
+    public function __invoke(Imagick $diff, Imagick $expected, Imagick $actual): ?string
+    {
+        if ($this->outputPath === null) {
+            return null;
+        }
+
+        // create directory
+        if (is_dir($this->outputPath) === false) {
+            mkdir($this->outputPath, 0777, true);
+        }
+
+        // write images and html
+        $diff->writeImage($this->outputPath . '/diff.png');
+        $expected->writeImage($this->outputPath . '/expected.png');
+        $actual->writeImage($this->outputPath . '/actual.png');
+        file_put_contents($this->outputPath . '/index.html', $this->createHtml());
+
+        return $this->outputPath . '/index.html';
+    }
+
+    private function createHtml(): string
+    {
+        return <<<HTML
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <style>
+        fieldset {
+            width: 300px;
+            float: left;
+        }
+
+        fieldset > img {
+            max-width: 100%;
+        }
+    </style>
+    <title>IsSameImageConstraint output</title>
+</head>
+<body>
+<fieldset>
+    <legend>Expected</legend>
+    <img src="expected.png" alt="expected">
+</fieldset>
+<fieldset>
+    <legend>Actual</legend>
+    <img src="actual.png" alt="actual">
+</fieldset>
+<fieldset>
+    <legend>Difference</legend>
+    <img src="diff.png" alt="diff">
+</fieldset>
+</body>
+</html>
+HTML;
+    }
+}
