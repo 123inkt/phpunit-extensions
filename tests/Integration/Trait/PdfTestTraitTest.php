996af0b5e72be9746d2014af35d645ab50938282
Frank Dekker <fdekker@123inkt.nl>
2026-09-11 11:17:40 +0200

    Add support for TcLibPdf in PdfTestTrait

--- tests/Integration/Trait/PdfTestTraitTest.php
+++ tests/Integration/Trait/PdfTestTraitTest.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace DR\PHPUnitExtensions\Tests\Integration\Trait;
 
+use Com\Tecnick\Pdf\Tcpdf as TcLibTcpdf;
 use DR\PHPUnitExtensions\Trait\PdfTestTrait;
 use PHPUnit\Framework\Attributes\CoversTrait;
 use PHPUnit\Framework\Attributes\RequiresPhpExtension;
 use PHPUnit\Framework\TestCase;
 use SplFileInfo;
-use TCPDF;
+use TCPDF as LegacyTcpdf;
 
 #[CoversTrait(PdfTestTrait::class)]
 #[RequiresPhpExtension('imagick')]
@@ -23,41 +24,66 @@ class PdfTestTraitTest extends TestCase
     public function testAssertSamePdf(): void
     {
         $pdf = new SplFileInfo($this->successPdfFile);
-        static::assertSamePdf($pdf, $this->createPdf());
+        static::assertSamePdf($pdf, $this->createLegacyPdf());
     }
 
     public function testAssertNotSamePdf(): void
     {
         $pdf = new SplFileInfo($this->failedPdfFile);
-        static::assertNotSamePdf($pdf, $this->createPdf());
+        static::assertNotSamePdf($pdf, $this->createLegacyPdf());
     }
 
     public function testAssertOnePdfIsEqualToTheOther(): void
     {
-        $pdfA = $this->createPdf();
-        $pdfB = $this->createPdf();
+        $pdfA = $this->createLegacyPdf();
+        $pdfB = $this->createLegacyPdf();
         static::assertSamePdf($pdfA, $pdfB);
     }
 
     public function testAssertOnePdfIsNotSameToTheOther(): void
     {
-        $pdfA = $this->createPdf();
-        $pdfB = $this->createPdf();
+        $pdfA = $this->createLegacyPdf();
+        $pdfB = $this->createLegacyPdf();
         $pdfB->writeHTML('<b>Changed</b>');
         static::assertNotSamePdf($pdfA, $pdfB);
     }
 
-    private function createPdf(): TCPDF
+    public function testAssertOneTcLibPdfIsEqualToTheOther(): void
     {
-        $pdf = new TCPDF('P', 'mm', [72, 100]);
-        $pdf->setAuthor("Author");
-        $pdf->setCreator("Creator");
-        $pdf->setPDFVersion("1.6");
+        $pdfA = $this->createTcLibPdf();
+        $pdfB = $this->createTcLibPdf();
+        static::assertSamePdf($pdfA, $pdfB);
+    }
+
+    public function testAssertOneTcLibPdfIsNotSameToTheOther(): void
+    {
+        $pdfA = $this->createTcLibPdf();
+        $pdfB = $this->createTcLibPdf(true);
+        static::assertNotSamePdf($pdfA, $pdfB);
+    }
+
+    private function createLegacyPdf(): LegacyTcpdf
+    {
+        $pdf = new LegacyTcpdf('P', 'mm', [72, 100]);
+        $pdf->setAuthor('Author');
+        $pdf->setCreator('Creator');
+        $pdf->setPDFVersion('1.6');
         $pdf->setLeftMargin(0);
         $pdf->setRightMargin(0);
         $pdf->setTopMargin(0);
         $pdf->AddPage();
         $pdf->writeHTML('<b>Foobar</b>');
+
+        return $pdf;
+    }
+
+    private function createTcLibPdf(bool $withRectangle = false): TcLibTcpdf
+    {
+        $pdf = new TcLibTcpdf();
+        $pdf->addPage();
+        if ($withRectangle) {
+            $pdf->page->addContent("0 0 100 100 re f\n");
+        }
 
         return $pdf;
     }
